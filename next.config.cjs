6838ab80ea4ece582c875a741a2075b82c12c60d
Michele-Carter <tumeke4u@gmail.com>
2024-09-18 17:19:56 +1200

    modified:   next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Ensures Next.js builds a static export
+  output: 'export', // Ensures Next.js builds a static export
+  distDir: ".next"
   };
   
   module.exports = nextConfig;
